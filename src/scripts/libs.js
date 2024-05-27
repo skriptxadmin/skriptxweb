5d23ec0e469b19ae7e1c45fdd41d9c1af3607582
skriptxadmin <mailtoskriptx@gmail.com>
2024-05-27 07:39:09 +0530

    home page contents added
    added contents for the homepage

--- src/scripts/libs.js
+++ src/scripts/libs.js
@@ -1,1 +1,11 @@
 require('bootstrap');
+
+const WOW = require('wowjs').WOW;
+
+const wowOptions = {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animate__animated', 
+}
+
+new WOW().init(wowOptions);
+
